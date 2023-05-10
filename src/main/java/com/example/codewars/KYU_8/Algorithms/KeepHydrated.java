3d793df2f1355b4c659a657e7d65d5ff2231375a
@JamshidYuldashev <jamshidyuldashev7799@gmail.com>
2023-05-10 21:51:26 +0500

    KYU_8 Algorithms, KeepHydrated, done

---
+++ src/main/java/com/example/codewars/KYU_8/Algorithms/KeepHydrated.java
@@ -0,0 +1,11 @@
+package com.example.codewars.KYU_8.Algorithms;
+
+public class KeepHydrated {
+    public static void main(String[] args) {
+        System.out.println(Liters(0.97));
+    }
+
+    public static int Liters(double time)  {
+        return (int) (time * 0.5);
+    }
+}
